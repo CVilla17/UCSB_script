5bb732403f32f1e147d4edbd1972f08b6d3dd762
CVilla17 <95596724+CVilla17@users.noreply.github.com>
2022-07-13 12:41:26 -0500

    Add files via upload

---
+++ UCSB_script.py
@@ -0,0 +1,179 @@
+import cript
+from getpass import getpass
+import requests
+import os
+import pandas as pd
+import json
+import yaml
+
+def load_config():
+    try:
+        with open("config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+    except FileNotFoundError:
+        config = {}
+
+    if config.get("host") is None:
+        config["host"] = input("Host (e.g., criptapp.org): ")
+    if config.get("token") is None:
+        config["token"] = getpass("API Token: ")
+    if config.get("group") is None:
+        config["group"] = input("Group name: ")
+    if config.get("collection") is None:
+        config["collection"] = input("Collection name: ")
+    if config.get("inventory") is None:
+        config["inventory"] = input("Inventory name: ")
+    if config.get("public") is None:
+        # Prompt user for privacy setting
+        config["public"] = input("Do you want your data visible to the public? (y/N): ").lower() == "y"
+    if config.get("path") is None:
+        config["path"] = input("Path to CSV file: ").strip('"')
+
+    return config
+
+def _setattrs(obj, **kwargs):
+    for key, value in kwargs.items():
+        setattr(obj, key, value)
+
+def get_inventory(inventory_name, group, collection, api, public):
+    """Either creates or retrieves an inventory object from CRIPT.
+    @args:
+    inventory_name:string
+    group:cript group object
+    collection:cript collection object
+    api:cript api object
+    public:boolean
+
+    @return:cript inventory object
+    """
+
+    inventory = cript.Inventory(group=group, collection=collection, name=inventory_name, materials=[], public=public)
+
+    # Save inventory
+    try:
+        api.save(inventory, max_level=0)
+        print(f"Created Inventory: {inventory.name}")
+    except cript.exceptions.DuplicateNodeError:
+        # Fetch inventory from the DB if it already exists
+        inventory = api.get(cript.Inventory, {"name": inventory.name, "group": group.uid}, max_level=0)
+        print(f"Found existing inventory: {inventory.name}")
+
+    return inventory
+
+def get_polymer(index,row,api,group,public):
+    """Creates or receives a cript Material object.
+    @args:
+    index: int
+    row: pandas DataSeries
+    api: cript api object
+    group: cript group object
+    public: boolean
+    @return: cript material object
+    """
+
+    #Creates polymer name from the monomers that comprise it
+    mono1_name=row["name1"]
+    mono2_name=row["name2"]
+    poly_name=f'{mono1_name}-db-{mono2_name}_({str(index+1)})'
+
+    #Create identifier objects for polymer
+    idpol=[cript.Identifier(key="preferred_name", value=poly_name),
+    cript.Identifier(key="bigsmiles", value=row["BIGSMILES"])]
+    
+    notes={}
+
+    mono1_info={"name":mono1_name,
+    "N":row["N1"],
+    "v(nm3)":row["v1(nm3)"],
+    "Rg(nm)":row["Rg1(nm)"]}
+
+    mono2_info={"name":mono2_name,
+    "N":row["N1"],
+    "v(nm3)":row["v2(nm3)"],
+    "Rg(nm)":row["Rg2(nm)"]}
+
+    other_info={"Lapprox":row["Lapprox"],
+    "nchains":row["nchains"],
+    "Vbox(nm3)":row["Vbox(nm3)"],
+    "T(K)":row["T(K)"],
+    "chi(vref=0.1nm3)":row["chi(vref=0.1nm3)"],
+    "chistd":row["chistd"],
+    "directory":row["directory"]
+    }
+
+    notes={"component1":mono1_info, "component2":mono2_info}
+    notes.update(other_info)
+    notes=json.dumps(notes)
+    
+
+    poly_dict={"group": group,
+    "name": poly_name,
+    "identifiers": idpol,
+    "notes": notes,
+    "public": public}
+
+    polymer = cript.Material(**poly_dict)
+
+    # Save material
+    try:
+        api.save(polymer, max_level=0)
+        print(f"ROW {index + 1} -- Created polymer: {polymer.name}")
+    except cript.exceptions.DuplicateNodeError:
+        # Fetch and update existing material
+        polymer = api.get(cript.Material, {"name": polymer.name, "created_by": api.user.uid}, max_level=0)
+        _setattrs(polymer, **poly_dict)
+        api.save(polymer, max_level=0)
+        print(f"ROW {index + 2} -- Updated existing polymer: {polymer.name}")
+
+    return polymer
+
+def parseFile(path,inventory,group,api,public):
+    """Iterates through rows and calls upon get_polymer to make material objects.
+    Adds materials to specified inventory.
+    @args:
+    path: str, file path
+    inventory:cript inventory object
+    group: cript group object
+    api:cript api object
+    public:boolean
+    @return: None
+    """
+    df=pd.read_csv(path)
+    
+    for index,row in df.iterrows():
+        polymer=get_polymer(index,row,api,group,public)
+        inventory.materials.append(polymer)
+
+    api.save(inventory, max_level=0)
+
+
+        
+        
+
+
+
+       
+        
+
+
+
+if __name__ == "__main__":
+    
+    config=load_config()
+    # Establish connection with the API
+    api = cript.API(config["host"], config["token"])
+
+    # Fetch objects
+    group = api.get(cript.Group, {"name": config["group"]}, max_level=0)
+    
+    collection = api.get(cript.Collection, {"name": config["collection"], "group": group.uid}, max_level=0)
+
+    inventory=get_inventory(
+        config["inventory"],
+        group,
+        collection,
+        api,
+        config["public"]
+        )
+
+    parseFile(config["path"],inventory,group,api,config["public"])
